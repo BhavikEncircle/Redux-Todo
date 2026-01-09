3be97f77123156791a3c9e0f1dbb21e4f188a3bf
BhavikEncircle <bhavikp.encircle@gmail.com>
2026-01-09 17:28:10 +0530

    Initiated Redux-Todo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AddTodo from "./AddTodo";
+import Todos from "./Todos";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-400 py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold text-gray-800 text-center mb-8">
+          Redux Todo App
+        </h1>
+        <AddTodo />
+        <Todos />
+      </div>
+    </div>
+  );
+};
+
+export default App;
